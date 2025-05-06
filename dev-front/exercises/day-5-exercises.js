a9da1a55d7a6b7f44f0827436b46c11f61724689
Hazel <basile.cunuder@gmail.com>
2025-05-06 21:36:42 +0200

    feat: add Use of variable exercises

---
+++ dev-front/exercises/day-5-exercises.js
@@ -0,0 +1,33 @@
+"use strict"
+
+// Exercices from Cyril
+
+// Use of variables 
+
+// Exercise 1 - show Hello World
+
+alert("Hello World");
+
+// Exercise 2 - Create Age variable + show it
+
+let age;
+
+alert(age);
+
+// Exercise 3 - Ask user to prompt their age + show it
+
+let age = prompt("How old are you?", "");
+
+alert(age);
+
+// Exercise 4 - Ask user to prompt circle radius + show it
+
+let circleRadius = prompt("What's the radius of the circle?","");
+
+alert(circleRadius);
+
+// Exercise 5 - Set 2 prompted values in 2 variables + reverse said variables
+
+let a = prompt("value a", ""), b = prompt("value b");
+
+let a = b, b = a
