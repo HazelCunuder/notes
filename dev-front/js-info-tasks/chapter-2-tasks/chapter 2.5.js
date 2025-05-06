d2698578458b7a094860b97967707a01b30990ee
Hazel <basile.cunuder@gmail.com>
2025-05-06 16:12:44 +0200

    feat(chapter-2.5.js): add exercises from chapter 2.5

---
+++ dev-front/js-info-tasks/chapter-2-tasks/chapter 2.5.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// Exercice js.info 2.5
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // Output: "hello 1"
+
+alert( `hello ${"name"}` ); // Output: "hello name
+
+alert( `hello ${name}` ); // Output: "hello Ilya"
