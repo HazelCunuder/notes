9f89622ab2d85df43880adc179b5ae2aff4060d2
Hazel <basile.cunuder@gmail.com>
2025-05-07 11:50:01 +0200

    feat(chapter-4.1.js): add task 3 and 4

--- dev-front/exercises/js-info-tasks/chapter-4-tasks/chapter-4.1.js
+++ dev-front/exercises/js-info-tasks/chapter-4-tasks/chapter-4.1.js
@@ -33,3 +33,61 @@ function isEmpty(obj) {
     }
     return true;
 }
+
+/*
+    Task 3
+
+    Object storing team salaries:
+    let salaries = {
+        John: 100,
+        Ann: 160,
+        Pete: 130
+    }
+
+    Write code to sum all slaraies and store in sum variable. Total should be 390
+    If salaries is empty, then result = 0
+*/
+
+let sum = 0;
+
+for(let key in salaries) {
+    sum += salaries[key];
+}
+
+console.log(sum);
+
+/* 
+    Task 4
+
+    Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+    For instance:
+
+    // before the call
+    let menu = {
+      width: 200,
+      height: 300,
+      title: "My menu"
+    };
+
+    multiplyNumeric(menu);
+
+    // after the call
+    menu = {
+      width: 400,
+      height: 600,
+      title: "My menu"
+    };
+
+    Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+    P.S. Use typeof to check for a number here.
+*/
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2
+        }
+    }
+};
