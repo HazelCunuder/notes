a66db1f3df5a455782865e457c7e61772bcb06a0
Hazel <basile.cunuder@gmail.com>
2025-05-09 11:19:17 +0200

    feat(chapter-5.4.js): add Task 1 from chapter 5.4

---
+++ dev-front/exercises/js-info-tasks/chapter-5-tasks/chapter-5.4.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// Tasks js.info 5.4 - Arrays
+
+// Task 1: Is array copied ?
+
+/// What is this code going to show?
+
+let fruits = ["Apples", "Pear", "Orange"];
+
+// push a new value into the "copy"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// what's in fruits?
+alert( fruits.length ); // 4
+
+
