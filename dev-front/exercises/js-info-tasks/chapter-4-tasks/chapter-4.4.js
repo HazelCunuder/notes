3d3ea6a25c23a879c3f8c43265a17de5a3700ba7
Hazel <basile.cunuder@gmail.com>
2025-05-09 10:14:17 +0200

    feat(chapter-4.4.js): add Task 1

---
+++ dev-front/exercises/js-info-tasks/chapter-4-tasks/chapter-4.4.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// Tasks js.info 4.4
+
+// Task 1: Using this in object literal
+
+/* 
+Here the function makeUser returns an object.
+
+What is the result of accessing its ref? Why?
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this
+  };
+}
+
+let user = makeUser();
+
+alert( user.ref.name ); // Error: Cannot read property 'name' of undefined
+*/
+
+// Task 2: Create a calculator
+
+/*
+Create a calculator object with three methods:
+- read() prompts for two values and saves them as object properties.
+- sum() returns the sum of saved values.
+- mul() returns the multiplication product of saved values.
+*/
