44e938e2a10801c342c84cb8c143f1925c706a66
Hazel <basile.cunuder@gmail.com>
2025-05-09 11:46:20 +0200

    feat(chapter-5.4.js): add Task 4

--- dev-front/exercises/js-info-tasks/chapter-5-tasks/chapter-5.4.js
+++ dev-front/exercises/js-info-tasks/chapter-5-tasks/chapter-5.4.js
@@ -51,3 +51,34 @@ The call arr[2]() is syntactically the good old obj[method](), in the role of ob
 So we have a call of the function arr[2] as an object method. Naturally, it receives this referencing the object arr and outputs the array:
 */
 
+// Task 4: Sum input numbers
+
+/*
+Write the function sumInput() that:
+
+    Asks the user for values using prompt and stores the values in the array.
+    Finishes asking when the user enters a non-numeric value, an empty string, or presses “Cancel”.
+    Calculates and returns the sum of array items.
+
+P.S. A zero 0 is a valid number, please don’t stop the input on zero.
+*/
+
+function sumInput() {
+    let numbers = [];
+
+    while (true) {
+        let value = prompt("Enter a number", 0);
+
+        if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+    }
+
+    let sum = 0
+    for (let number of numbers) {
+        sum += number;
+    }
+    return sum
+}
+
+console.log(sumInput())
